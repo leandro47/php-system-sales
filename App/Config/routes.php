79ede9ac96e6c9c1eb26fb880ec0a99bd36a01eb
leandro47 <leandrosilva47@live.com>
2021-01-21 19:42:23 -0300

    :bug: autoload class ps4

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -1,5 +1,12 @@
 <?php
 
+use Pecee\SimpleRouter\SimpleRouter;
+
+SimpleRouter::setDefaultNamespace('App\Controllers');
+
+SimpleRouter::get('/', 'ProductController@index');
+
+
 
 
 
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__. '/App/Config/routes.php';
+require __DIR__ . '/App/Config/routes.php';
 
+use Pecee\SimpleRouter\SimpleRouter;
+SimpleRouter::start();
